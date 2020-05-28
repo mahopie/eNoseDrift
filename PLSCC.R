7b53ea8ee29fd12cc59f52f10b9cff380f059c67
mahopie <36864849+mahopie@users.noreply.github.com>
2020-05-28 10:34:40 +0200

    Update PLSCC.R

--- PLSCC.R
+++ PLSCC.R
@@ -13,7 +13,7 @@ PLSCC_tune <- function(X,y,ncomp=2){
   # Parameters
   # X: Calibrant data matrix (n_obs x n_sens)
   # y: time (n_obs x 1)
-  # ncomp: Number components to return
+  # ncomp: Number of components to return
   #
   # Return
   # ncomp drift latent directions
@@ -28,18 +28,18 @@ PLSCC_cor <- function(X,pls,ncomp=2){
   #
   # Parameters
   # X: Drifted data matrix (n_obs x n_sens)
-  # pls: PLS object containing the latent directions to remove
+  # pls: PLS object (from PLSCC_tune) containing the latent directions to remove
   #
   # Return
   # Corrected data matrix
-  Xcor <- X
-  ncomp <- ncol(pls$loadings)
-  for(i in 1:ncomp){
-    wd <- matrix(pls$weights[,i])
-    pd <- matrix(pls$loadings[,i])
-    td <- Xcor %*% wd %*% ginv(t(pd) %*% wd)
-    Xcor <- Xcor - td %*% t(pd)
-  }
-  return(Xcor)
+  # Xcor <- X
+  # ncomp <- ncol(pls$loadings)
+  # for(i in 1:ncomp){
+  #   wd <- matrix(pls$weights[,i])
+  #   pd <- matrix(pls$loadings[,i])
+  #   td <- Xcor %*% wd %*% ginv(t(pd) %*% wd)
+  #   Xcor <- Xcor - td %*% t(pd)
+  # }
+  return(X%*%(diag(rep(1,ncol(X))) - pls$weights %*% ginv(t(pls$loadings)%*%pls$weights) %*% t(pls$loadings)))
 }
 ###########################
