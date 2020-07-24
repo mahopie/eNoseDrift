40a22d3fba39926a94934a9f9af11bfcc55cbb3d
mahopie <36864849+mahopie@users.noreply.github.com>
2020-07-24 15:05:23 +0200

    Update PCACC.R

--- PCACC.R
+++ PCACC.R
@@ -6,17 +6,23 @@
 
 ###########################
 # Principal Component Analysis
-PCACC_tune <- function(X,ncomp=2){
+PCACC_tune <- function(X,ncomp=NULL,varExp=0.95){
   # Principal Component Analysis - Component Correction from Artursson et al.
   # Tom Artursson, Tomas Eklöv, Ingemar Lundström, et al. “Drift correction for gas sensors using multivariate methods”. In: Journal of Chemometrics (Sept. 2000)
   #
   # Parameters
   # X: Calibrant data matrix (n_obs x n_sens)
-  # ncomp: Number of components to return
+  # ncomp: Number components to return
+  # varExp: minimal number of components to explain varExp variance in the calibrant matrix
   #
   # Return
   # ncomp drift directions
   pca <- prcomp(X)
+  if(is.null(ncomp)){
+    var_comp = cumsum(pca$sdev**2)/sum(pca$sdev**2)
+    ncomp = which(var_comp > varExp)[1]
+    print(paste(ncomp,'components have been chosen'))
+  }
   return(matrix(pca$rotation[,1:ncomp],ncol=ncomp))
 }
 
